71bd552b2507b1beb97e37736641d7f155c28819
Koda <andeyaelydio@gmail.com>
2024-11-01 10:58:56 -0300

    Final

--- TV/index.js
+++ TV/index.js
@@ -4,21 +4,18 @@ const multer = require('multer');
 const path = require('path');
 
 const app = express();
-const upload = multer(); // Você pode configurar o armazenamento do multer se necessário
+const upload = multer(); 
 const port = 3030;
 
-// Aumenta o limite para 50mb
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rotas
 app.use('/', require('./routes/dashboard/index.js'));
 app.use('/', require('./routes/admin/index.js'));
 app.use('/', require('./routes/feedback/index.js'));
 
-// Inicia o servidor
 app.listen(port, () => {
     console.log(`🧨 | Servidor rodando em: http://localhost:${port} !`);
 });
